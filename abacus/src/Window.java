b1d3a03f029e3f8a99f59a958edaadc0e018a7a7
jpmchatton98 <jpmchatton98@gmail.com>
2020-09-17 10:33:37 -0600

    Manual GridBag creation

--- abacus/src/Window.java
+++ abacus/src/Window.java
@@ -1,90 +1,60 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.lang.reflect.Method;
+import java.awt.*;
 
 public class Window
 {
 	private static final int HISTORY_LEN = 10;
 
-	private JTextField equationInput;
-	private JButton submit;
-	private JPanel window;
+	private static JTextField equationInput;
+	private static JButton submit;
+	private static JPanel window;
 
-	public JLabel history0;
-	public JLabel history1;
-	public JLabel history2;
-	public JLabel history3;
-	public JLabel history4;
-	public JLabel history5;
-	public JLabel history6;
-	public JLabel history7;
-	public JLabel history8;
-	public JLabel history9;
-	public JLabel answer0;
-	public JLabel answer1;
-	public JLabel answer2;
-	public JLabel answer3;
-	public JLabel answer4;
-	public JLabel answer5;
-	public JLabel answer6;
-	public JLabel answer7;
-	public JLabel answer8;
-	public JLabel answer9;
-
-	public JLabel[] history;
+	public static JLabel[] history;
+	public static JLabel[] answers;
 
 	public static void main(String[] args)
 	{
-		JFrame frame = new JFrame("testForm");
-		frame.setContentPane(new Window().window);
+		history = new JLabel[HISTORY_LEN];
+		answers = new JLabel[HISTORY_LEN];
+
+		window = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+
+		for(int i = 0; i < HISTORY_LEN; i++)
+		{
+			history[i] = new JLabel("history" + i);
+			c.weighty = 1;
+			c.weightx = 1;
+			c.gridy = i;
+			c.gridx = 0;
+			window.add(history[i], c);
+			c.weighty = 0;
+
+			answers[i] = new JLabel("answer" + i);
+			c.gridx = 1;
+			c.weightx = 0;
+			window.add(answers[i], c);
+		}
+
+		equationInput = new JTextField();
+		c.gridx = 0;
+		c.gridy = HISTORY_LEN;
+		c.weightx = 1;
+		c.fill = 1;
+		window.add(equationInput, c);
+
+		submit = new JButton("˄");
+		c.gridx = 1;
+		c.fill = 0;
+		c.weightx = 0;
+		window.add(submit, c);
+
+		JFrame frame = new JFrame("test window");
+		frame.setContentPane(window);
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
 		frame.setSize(500, 500);
-		frame.setResizable(false);
 		frame.setVisible(true);
+		frame.setResizable(false);
 	}
-
-	private class equationListener implements ActionListener
-	{
-		public void actionPerformed(ActionEvent e)
-		{
-			String equation = equationInput.getText();
-
-			for(int i = 0; i < 10; i++)
-			{
-				try
-				{
-					Object instance = getClass().getDeclaredField("history" + i).get(this);
-					Method m = instance.getClass().getMethod("split", String.class);
-
-					Object returnValue = m.invoke(instance, ";");
-					if(returnValue instanceof JLabel)
-					{
-						System.out.println((JLabel)returnValue);
-					}
-				}
-				catch(Exception exc)
-				{
-					exc.printStackTrace();
-				}
-			}
-		}
-	}
-
-	/*// If a history label is clicked, make the current equation that historical equation
-	private class historyFill implements MouseListener
-	{
-		public void mouseClicked(MouseEvent e)
-		{
-			Label source = (Label) e.getSource();
-
-			equationInput.setText(source.getText());
-		}
-
-		public void mousePressed(MouseEvent e){}
-		public void mouseReleased(MouseEvent e){}
-		public void mouseEntered(MouseEvent e){}
-		public void mouseExited(MouseEvent e){}
-	}*/
 }

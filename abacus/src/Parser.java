4b29c3b641d890df10837823e7d20962c693b3ed
jpmchatton98 <jpmchatton98@gmail.com>
2020-09-17 11:27:37 -0600

    Added replacement functions

--- abacus/src/Parser.java
+++ abacus/src/Parser.java
@@ -7,6 +7,39 @@ public class Parser
 	// EquationPart objects that can be compiled into the answer.
 	public PriorityQueue<EquationPart> parse(String equation)
 	{
+		// Replace all mutli-character strings with single characters
+
+		equation = equation.replace("sqrt", "√");
+		equation = equation.replace("cbrt", "∛");
+		equation = equation.replace("pi", "π");
+
+		// Trigonometric function logic:
+		//      - Basic functions (sin, cos, tan) are lowercase
+		//      - Complex functions (sec, csc, cot) are uppercase
+		//      - Arc functions have cedillas
+		//      - Hyperbolic functions have carons
+
+		equation = equation.replace("sin", "s");
+		equation = equation.replace("cos", "c");
+		equation = equation.replace("tan", "t");
+		equation = equation.replace("sec", "S");
+		equation = equation.replace("csc", "C");
+		equation = equation.replace("cot", "T");
+
+		equation = equation.replace("asin", "ç");
+		equation = equation.replace("acos", "ş");
+		equation = equation.replace("atan", "ţ");
+		equation = equation.replace("asec", "Ş");
+		equation = equation.replace("acsc", "Ç");
+		equation = equation.replace("acot", "Ţ");
+
+		equation = equation.replace("sinh", "š");
+		equation = equation.replace("cosh", "č");
+		equation = equation.replace("tanh", "ť");
+		equation = equation.replace("sech", "Š");
+		equation = equation.replace("csch", "Č");
+		equation = equation.replace("coth", "Ť");
+
 		// Change the equation string to an array
 		char[] equationArray = equation.toCharArray();
 

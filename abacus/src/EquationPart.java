c33d948fff98c0a652b5feb7b3e0d8884a016f1e
jpmchatton98 <jpmchatton98@gmail.com>
2020-09-29 00:35:54 -0600

    Implemented roots and multi-character functions.  Also fixed compilation function

--- abacus/src/EquationPart.java
+++ abacus/src/EquationPart.java
@@ -3,31 +3,18 @@ public class EquationPart
 	private double number;   // The number to perform the function on (e.g. 3, 5, 7, etc)
 	private String function; // The function to perform on the number (e.g. +, -, *, etc)
 
-	private EquationPart nextPart = null; // The next part in the equation
-	private EquationPart prevPart; // The previous part in the equation
-
-	EquationPart(double number, EquationPart prevPart)
+	EquationPart(double number)
 	{
 		this.number = number;
-		this.prevPart = prevPart;
 	}
-	EquationPart(String function, EquationPart prevPart)
+	EquationPart(String function)
 	{
 		this.function = function;
-		this.prevPart = prevPart;
 	}
-	EquationPart(double number, String function, EquationPart prevPart)
+	EquationPart(double number, String function)
 	{
 		this.number = number;
 		this.function = function;
-		this.prevPart = prevPart;
-	}
-	EquationPart(double number, String function, EquationPart prevPart, EquationPart nextPart)
-	{
-		this.number = number;
-		this.function = function;
-		this.prevPart = prevPart;
-		this.nextPart = nextPart;
 	}
 
 	public double getNumber()
@@ -46,22 +33,5 @@ public class EquationPart
 	public void setFunction(String function)
 	{
 		this.function = function;
-	}
-
-	public EquationPart prev()
-	{
-		return prevPart;
-	}
-	public EquationPart next()
-	{
-		return nextPart;
-	}
-	public void setPrev(EquationPart part)
-	{
-		prevPart = part;
-	}
-	public void setNext(EquationPart part)
-	{
-		nextPart = part;
 	}
 }
--- abacus/src/Parser.java
+++ abacus/src/Parser.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import static java.lang.Character.MATH_SYMBOL;
 import static java.lang.Character.isDigit;
 import static java.lang.Double.NaN;
 import static java.lang.Double.parseDouble;
@@ -7,9 +10,98 @@ import static java.lang.Double.parseDouble;
 // This class contains code for the core parsing algorithm
 public class Parser
 {
-	// Core parsing function.  Accepts the equation string as an input and outputs a queue of
-	// EquationPart objects that can be compiled into the answer.
+	// Core parsing function.  Accepts the equation string as an input and outputs the answer
+	// in the form of a double.
 	public double parse(String equation)
+	{
+		try
+		{
+			equation = prepareEquation(equation);
+			ArrayList<EquationPart> equationParts = new ArrayList<>();
+
+			// Change the equation string to an array
+			String number = "";
+			String function = "";
+			EquationPart curr = null;
+
+			boolean startingNegative = false;
+			int mode = 0; // 0: Number, 1: Function
+
+			for (int i = 0; i < equation.length(); i++)
+			{
+				if (i == 0 && equation.charAt(i) == '-') // Starting negative
+				{
+					startingNegative = true;
+					continue;
+				}
+
+				if (isNumberOrPeriod(equation.charAt(i)))
+				{
+					if (mode == 1)
+					{
+						if (function.equals(""))
+						{
+							function = "+";
+						}
+
+						curr = new EquationPart(function);
+						equationParts.add(curr);
+						curr = null;
+
+						function = "";
+					}
+
+					mode = 0;
+					number += equation.charAt(i);
+				}
+				else
+				{
+					if (mode == 0)
+					{
+						if (equationParts.size() > 0)
+						{
+							equationParts.get(equationParts.size() - 1).setNumber(parseDouble(number));
+						}
+						else // First Element
+						{
+							if (startingNegative)
+							{
+								number = "-" + number;
+							}
+							equationParts.add(new EquationPart(parseDouble(number), "+"));
+						}
+						number = "";
+					}
+
+					mode = 1;
+					function += equation.charAt(i);
+				}
+			}
+
+			if (equationParts.size() > 0)
+			{
+				equationParts.get(equationParts.size() - 1).setNumber(parseDouble(number));
+			}
+			else
+			{
+				if (startingNegative)
+				{
+					number = "-" + number;
+				}
+				return parseDouble(number);
+			}
+
+			return compileEquation(equationParts);
+		}
+		catch(NumberFormatException e)
+		{
+			return NaN;
+		}
+	}
+
+	// Prepares the equation for parsing by trimming out spaces and replacing multi-character
+	// functions with single characters.
+	private String prepareEquation(String equation)
 	{
 		//Trim out all of the spaces
 		equation = equation.replace(" ", "");
@@ -48,215 +140,76 @@ public class Parser
 		equation = equation.replace("csc", "C");
 		equation = equation.replace("cot", "T");
 
-		ArrayList<EquationPart> equationParts = new ArrayList<>();
-
-		// Change the equation string to an array
-		char[] equationArray = equation.toCharArray();
-		String curr = "";
-		String function = "";
-
-		if(equationArray[0] != '-')
-		{
-			// Core parsing loop
-			for (int i = 0; i < equationArray.length; i++)
-			{
-				if (isNumberOrPeriod(equationArray[i]))
-				{
-					curr += equationArray[i];
-				}
-				else
-				{
-					if (equationParts.size() > 0)
-					{
-						equationParts.add(new EquationPart(parseDouble(curr), function, equationParts.get(equationParts.size() - 1)));
-						equationParts.get(equationParts.size() - 2).setNext(equationParts.get(equationParts.size() - 1));
-					}
-					else
-					{
-						equationParts.add(new EquationPart(parseDouble(curr), "+", null));
-					}
-					curr = "";
-					function = equationArray[i] + "";
-				}
-			}
-		}
-		else
-		{
-			// Core parsing loop
-			for (int i = 1; i < equationArray.length; i++)
-			{
-				if (isNumberOrPeriod(equationArray[i]))
-				{
-					curr += equationArray[i];
-				}
-				else
-				{
-					if (equationParts.size() > 0)
-					{
-						equationParts.add(new EquationPart(parseDouble(curr), function, equationParts.get(equationParts.size() - 1)));
-						equationParts.get(equationParts.size() - 2).setNext(equationParts.get(equationParts.size() - 1));
-					}
-					else
-					{
-						equationParts.add(new EquationPart(parseDouble(curr), "-", null));
-					}
-					curr = "";
-					function = equationArray[i] + "";
-				}
-			}
-		}
-
-		if(equationParts.size() < 1)
-		{
-			equationParts.add(new EquationPart(parseDouble(curr), function, equationParts.get(equationParts.size() - 1)));
-			equationParts.get(equationParts.size() - 2).setNext(equationParts.get(equationParts.size() - 1));
-
-			return compileEquation(equationParts);
-		}
-		else
-		{
-			return parseDouble(curr);
-		}
-
-
+		return equation;
 	}
 
 	private double compileEquation(ArrayList<EquationPart> equationParts)
 	{
-		EquationPart part = equationParts.get(0);
-
-		while (part != null && !(part.next() == null && part.prev() == null)) // Search for multiplication and division
+		int index = 0;
+		while(equationParts.size() > 1)
 		{
-			if (part.next() != null)
+			if(equationParts.get(index + 1).getFunction().equals("^") || equationParts.get(index + 1).getFunction().contains("√"))
 			{
-				if (part.next().getFunction().equals("^") || part.next().getFunction().contains("√"))
-				{
-					EquationPart newNext = part.next().next();
-					EquationPart newPrev = part.prev();
+				EquationPart part = equationParts.get(index);
+				EquationPart nextPart = equationParts.get(index + 1);
 
-					String newFunction = part.getFunction();
-					double newNumber = compileFunction(part.getNumber(), part.next().getNumber(), part.next().getFunction());
-
-					EquationPart newPart = new EquationPart(newNumber, newFunction, newPrev, newNext);
-					equationParts.add(newPart);
-
-					if (newNext != null)
-					{
-						newNext.setPrev(newPart);
-					}
-
-					equationParts.remove(part);
-					equationParts.remove(part.next());
-
-					part = newPart;
-				}
-
-				if (part.next() != null)
-				{
-					part = part.next();
-				}
-				else
-				{
-					break;
-				}
-			}
-			else
-			{
-				break;
-			}
-		}
-
-		// Rewind
-		while (part.prev() != null)
-		{
-			part = part.prev();
-		}
-
-		while (part != null && !(part.next() == null && part.prev() == null)) // Search for multiplication and division
-		{
-			if (part.next() != null)
-			{
-				if (part.next().getFunction().equals("*") || part.next().getFunction().equals("/") || part.getFunction().equals("%"))
-				{
-					EquationPart newNext = part.next().next();
-					EquationPart newPrev = part.prev();
-
-					String newFunction = part.getFunction();
-					double newNumber = compileFunction(part.getNumber(), part.next().getNumber(), part.next().getFunction());
-
-					EquationPart newPart = new EquationPart(newNumber, newFunction, newPrev, newNext);
-					equationParts.add(newPart);
-
-					if (newNext != null)
-					{
-						newNext.setPrev(newPart);
-					}
-
-					if (newPart.prev() != null)
-					{
-						newPart.prev().setNext(newPart);
-					}
-
-					equationParts.remove(part);
-					equationParts.remove(part.next());
-
-					part = newPart;
-				}
-
-				if (part.next() != null)
-				{
-					part = part.next();
-				}
-				else
-				{
-					break;
-				}
-			}
-			else
-			{
-				break;
-			}
-		}
-
-		// Rewind
-		while (part != null && part.prev() != null)
-		{
-			part = part.prev();
-		}
-
-		while (part != null && !(part.next() == null && part.prev() == null)) // Search for everything else
-		{
-			if (part.next() != null)
-			{
-				EquationPart newNext = part.next().next();
-				EquationPart newPrev = part.prev();
-
-				String newFunction = part.getFunction();
-				double newNumber = compileFunction(part.getNumber(), part.next().getNumber(), part.next().getFunction());
-
-				EquationPart newPart = new EquationPart(newNumber, newFunction, newPrev, newNext);
-				equationParts.add(newPart);
-
-				if (newNext != null)
-				{
-					newNext.setPrev(newPart);
-				}
+				double newNumber = compileFunction(part.getNumber(), nextPart.getNumber(), nextPart.getFunction());
+				EquationPart newPart = new EquationPart(newNumber, part.getFunction());
 
 				equationParts.remove(part);
-				equationParts.remove(part.next());
-
-				part = newPart;
-
-				part = part.next();
+				equationParts.remove(nextPart);
+				equationParts.add(index, newPart);
 			}
 			else
 			{
-				// Rewind
-				while (part.prev() != null)
-				{
-					part = part.prev();
-				}
+				index++;
 			}
+
+
+			if(index >= equationParts.size() - 1)
+			{
+				break;
+			}
+		}
+
+		index = 0;
+		while(equationParts.size() > 1)
+		{
+			if(equationParts.get(index + 1).getFunction().equals("*") || equationParts.get(index + 1).getFunction().equals("/"))
+			{
+				EquationPart part = equationParts.get(index);
+				EquationPart nextPart = equationParts.get(index + 1);
+
+				double newNumber = compileFunction(part.getNumber(), nextPart.getNumber(), nextPart.getFunction());
+				EquationPart newPart = new EquationPart(newNumber, part.getFunction());
+
+				equationParts.remove(part);
+				equationParts.remove(nextPart);
+				equationParts.add(index, newPart);
+			}
+			else
+			{
+				index++;
+			}
+
+			if(index >= equationParts.size() - 1)
+			{
+				break;
+			}
+		}
+
+		index = 0;
+		while(equationParts.size() > 1)
+		{
+			EquationPart part = equationParts.get(index);
+			EquationPart nextPart = equationParts.get(index + 1);
+
+			double newNumber = compileFunction(part.getNumber(), nextPart.getNumber(), nextPart.getFunction());
+			EquationPart newPart = new EquationPart(newNumber, part.getFunction());
+
+			equationParts.remove(part);
+			equationParts.remove(nextPart);
+			equationParts.add(index, newPart);
 		}
 
 		return equationParts.get(0).getNumber();
@@ -290,11 +243,6 @@ public class Parser
 			{
 				return num1 % num2;
 			}
-			case "√":
-			case "*√":
-			{
-				return num1 * Math.sqrt(num2);
-			}
 			case "+√":
 			{
 				return num1 + Math.sqrt(num2);
@@ -302,6 +250,11 @@ public class Parser
 			case "-√":
 			{
 				return num1 - Math.sqrt(num2);
+			}
+			case "√":
+			case "*√":
+			{
+				return num1 * Math.sqrt(num2);
 			}
 			case "/√":
 			{
